f4f22e7eeb2405819f76eda154659faefdbd756f
Dmikko <johanson.mike@hotmail.com>
2024-11-27 14:19:26 +0100

    Tilføjet værdier til locations og popups

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 cafesArray.forEach(function(sighting) {
     const lat = sighting.lat;
     const lng = sighting.lng;
-   /* const size=sighting.size;
+    const size=sighting.size;
     const price=sighting.price;
     const wifi=sighting.wifi;
     const discount=sighting.student_discount;
-    const music=sighting.music*/
+    const music=sighting.music
     console.log(sighting, lat, lng);
 
     L.marker([lat, lng]).addTo(map).bindPopup(`<b>${sighting.name}</b> <br> Størrelse:${sighting.size} <br> Pris:${sighting.price} <br> Wifi:${sighting.wifi} <br> Studierabat:${sighting.student_discount} <br> Musik:${sighting.music}`);
