d6b7e813b592ff02919e25ae8240ca36123d95a0
Dmikko <johanson.mike@hotmail.com>
2024-11-27 12:06:26 +0100

    main.js order

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 console.log("SkyNet initiated")
 
 
-//const cors = require("cors");
-//const mysql = require("mysql2");
-//const express = require("express");
 
 
 
@@ -27,3 +24,7 @@ cafesArray.forEach(function(sighting) {
 });
 
 console.log("Map initialized");
+
+const cors = require("cors");
+const mysql = require("mysql2");
+const express = require("express");
