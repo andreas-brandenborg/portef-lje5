40c09d21fbc7cbd602873cd30c0b67afe7193491
magnusgiemsa <magnus.giemsa@gmail.com>
2024-11-27 13:36:07 +0100

    post and get

--- main.js
+++ main.js
@@ -24,3 +24,35 @@ console.log("Map initialized");
 const cors = require("cors");
 const mysql = require("mysql2");
 const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password: process.env.psw,
+    database:"pokemon"
+});
+
+//localhost:4000/
+app.get('/all',(req,res)=>{
+    const queryParameter = req.query.type;
+    const q = "SELECT * FROM pokemon";
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
+
+//localhost:3000/new
+app.post('/new',(req,res)=>{
+    const name = req.body.name;
+    const primary = req.body.primary;
+    const q = `insert into pokemon (name, primary_type) values("${name}", "${primary}");`;
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
